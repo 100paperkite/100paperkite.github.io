da2f6d5ad4503e1baf4ba0db1a4768240858cafd
Baek Jiyeon <100.paperkite@gmail.com>
2022-03-17 01:54:49 +0900

    fix: Git 대소문자 문제로 인한 폴더 중복 해결

--- components/post/CodeBlock.jsx
+++
@@ -1,9 +0,0 @@
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { vs2015 } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
-
-const PostCodeBlock = ({ children: { props: props } }) => {
-  const { className: language } = props;
-  return <SyntaxHighlighter language={language} {...props} style={vs2015} />;
-};
-
-export default PostCodeBlock;
