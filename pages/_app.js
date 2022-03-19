c5660bc9ae126f77a6d9234a9d122cefc1113fbc
Baek Jiyeon <100.paperkite@gmail.com>
2022-03-19 23:02:30 +0900

    fix: Google Analytics 스크립트 수정 - 배포에서만 동작하도록 변경 - <head> 태그로 이동.

--- pages/_app.js
+++ pages/_app.js
@@ -1,45 +1,29 @@
 import '../styles/globals.css';
 import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 import Script from 'next/script';
+import { useRouter } from 'next/router';
 import { DefaultSeo } from 'next-seo';
-import * as gtag from '../lib/gtag';
 
+import * as gtag from '../lib/gtag';
 import site from '../siteMetadata';
 
 function App({ Component, pageProps }) {
   const router = useRouter();
+
   useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
+    if (process.env.NODE_ENV === 'production') {
+      const handleRouteChange = (url) => {
+        gtag.pageview(url);
+      };
+      router.events.on('routeChangeComplete', handleRouteChange);
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange);
+      };
+    }
   }, [router.events]);
 
   return (
     <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <DefaultSeo
         openGraph={{
           type: 'website',
--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,35 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 import site from '../siteMetadata';
+import * as gtag from '../lib/gtag';
 
 export default function Document() {
   return (
     <Html lang={site.locale}>
-      <Head />
+      <Head>
+        {process.env.NODE_ENV === 'production' && (
+          <>
+            <script
+              id="gtag-init"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+              }}
+            ></script>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+            />
+          </>
+        )}
+      </Head>
       <body>
         <Main />
         <NextScript />
