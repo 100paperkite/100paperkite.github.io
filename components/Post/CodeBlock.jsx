7e8d1a44cc342ecebcf2bcbb634eeb40a215f661
Baek Jiyeon <100.paperkite@gmail.com>
2022-03-27 18:03:03 +0900

    fix: markdown language name의 nullable 판단

--- components/Post/CodeBlock.jsx
+++ components/Post/CodeBlock.jsx
@@ -3,7 +3,7 @@ import { github as style } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 
 const CodeBlock = ({ children: { props } }) => {
   const { className, children: codeString } = props;
-  const language = className.split('-')[1]; // language-${language} 형태.
+  const language = className?.split('-')[1]; // language-${language} 형태.
   return (
     <SyntaxHighlighter language={language} style={style}>
       {codeString.trim()}
