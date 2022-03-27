afcaa2f72f3be926fa310346357019404ccac769
Baek Jiyeon <100.paperkite@gmail.com>
2022-03-27 18:48:33 +0900

    chore: 댓글 구분 선 추가

--- components/Post/Utterances.jsx
+++ components/Post/Utterances.jsx
@@ -1,23 +1,26 @@
 const Utterances = () => {
   return (
     // https://github.com/utterance/utterances/issues/161
-    <section
-      ref={(element) => {
-        if (!element) {
-          return;
-        }
+    <div>
+      <hr className="border-t border-neutral-300 my-8 sm:my-16" />
+      <section
+        ref={(element) => {
+          if (!element) {
+            return;
+          }
 
-        const scriptElement = document.createElement('script');
-        scriptElement.setAttribute('src', 'https://utteranc.es/client.js');
-        scriptElement.setAttribute('repo', '100paperkite/100paperkite.github.io');
-        scriptElement.setAttribute('issue-term', 'title');
-        scriptElement.setAttribute('label', '💬');
-        scriptElement.setAttribute('theme', 'github-light');
-        scriptElement.setAttribute('crossorigin', 'anonymous');
-        scriptElement.setAttribute('async', 'true');
-        element.replaceChildren(scriptElement);
-      }}
-    />
+          const scriptElement = document.createElement('script');
+          scriptElement.setAttribute('src', 'https://utteranc.es/client.js');
+          scriptElement.setAttribute('repo', '100paperkite/100paperkite.github.io');
+          scriptElement.setAttribute('issue-term', 'title');
+          scriptElement.setAttribute('label', '💬');
+          scriptElement.setAttribute('theme', 'github-light');
+          scriptElement.setAttribute('crossorigin', 'anonymous');
+          scriptElement.setAttribute('async', 'true');
+          element.replaceChildren(scriptElement);
+        }}
+      />
+    </div>
   );
 };
 
