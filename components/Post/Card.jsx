e30d79709ebf4e47e2c3e9230b8e4ca84d993915
Baek Jiyeon <100.paperkite@gmail.com>
2022-04-25 00:05:44 +0900

    :lipstick:

--- components/Post/Card.jsx
+++ components/Post/Card.jsx
@@ -13,7 +13,7 @@ const Card = ({ post: { frontMatter, slug } }) => {
   const isTagExist = (tags ?? []).length === 0 ? false : true;
 
   return (
-    <article className="my-4 p-3 px-4 md:px-5 md:p-4 rounded-2xl md:rounded-3xl bg-neutral-100">
+    <article className="my-4 p-3 px-4 md:px-5 md:p-4 rounded-2xl md:rounded-2xl bg-neutral-50">
       <div className="flex flex-wrap gap-x-1 gap-y-2 items-center text-sm text-neutral-500">
         <span className="mr-2 my-auto align-middle">{uploadedDate}</span>
         {isTagExist && (
@@ -25,13 +25,13 @@ const Card = ({ post: { frontMatter, slug } }) => {
         )}
       </div>
 
-      <h2 className="font-extrabold my-3 md:my-4 text-2xl md:text-3xl hover:text-neutral-600 hover:duration-150">
+      <h2 className="font-extrabold mt-2 md:mt-3 text-xl md:text-2xl hover:text-neutral-600 hover:duration-150">
         <Link href={`/posts/${slug}`}>
           <a>{title}</a>
         </Link>
       </h2>
 
-      <p className="my-1 md:my-2 text-neutral-500">{description}</p>
+      <p className="my-1 md:my-2 text-neutral-500 text-xs md:text-sm">{description}</p>
     </article>
   );
 };
--- components/Post/Tag.jsx
+++ components/Post/Tag.jsx
@@ -1,6 +1,6 @@
 const Tag = ({ name }) => {
   return (
-    <div className="text-xs rounded-2xl bg-neutral-300 text-neutral-600 font-semibold px-3.5 py-1">
+    <div className="text-[10px] md:text-xs rounded-full bg-neutral-200 text-neutral-500 font-semibold px-2.5 md:px-3.5 md:py-1">
       {name}
     </div>
   );
