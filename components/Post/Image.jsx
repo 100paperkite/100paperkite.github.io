e6c3defb83b9bb8b4effc249076fe4f3d9b2c11a
Baek Jiyeon <100.paperkite@gmail.com>
2023-01-08 21:37:14 +0900

    chore: 이미지 출처 보이도록 수정

--- components/Post/Image.jsx
+++ components/Post/Image.jsx
@@ -2,24 +2,21 @@ import NextImage from 'next/image';
 
 const Image = ({ src, alt, height, sourceHref, source }) => {
   return (
-    <figure
-      className="text-center pt-1 sm:pt-2 md:pt-3 relative w-full drop-shadow-xl"
-      style={{ height }}
-    >
-      <NextImage
-        className="flex"
-        src={src}
-        alt={alt}
-        layout="fill"
-        objectFit="contain"
-        unoptimized
-      />
-      <div>
-        <a className="text-xs underline text-neutral-500" href={sourceHref}>
-          {source || sourceHref}
-        </a>
-      </div>
-    </figure>
+    <div className="text-center pt-1 sm:pt-2 md:pt-3">
+      <figure className="relative w-full drop-shadow-xl" style={{ height }}>
+        <NextImage
+          className="flex"
+          src={src}
+          alt={alt}
+          layout="fill"
+          objectFit="contain"
+          unoptimized
+        />
+      </figure>
+      <a className=" text-xs underline text-neutral-500" href={sourceHref}>
+        {source || sourceHref}
+      </a>
+    </div>
   );
 };
 
