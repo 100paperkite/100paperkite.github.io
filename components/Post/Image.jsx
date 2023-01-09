d63656da11363324d360bdb89037471442ece48e
Baek Jiyeon <100.paperkite@gmail.com>
2023-01-09 18:52:20 +0900

    fix: 이미지 가로, 세로 크기 지정하게끔 변경

--- components/Post/Image.jsx
+++ components/Post/Image.jsx
@@ -1,18 +1,16 @@
 import NextImage from 'next/image';
 
-const Image = ({ src, alt, height, sourceHref, source }) => {
+const Image = ({ src, alt, width, height, sourceHref, source }) => {
   return (
     <div className="text-center pt-1 sm:pt-2 md:pt-3">
-      <figure className="relative w-full drop-shadow-xl" style={{ height }}>
-        <NextImage
-          className="flex"
-          src={src}
-          alt={alt}
-          layout="fill"
-          objectFit="contain"
-          unoptimized
-        />
-      </figure>
+      <NextImage
+        className="max-w-full h-auto"
+        src={src}
+        alt={alt}
+        width={width || 700}
+        height={height}
+        unoptimized
+      />
       <a className=" text-xs underline text-neutral-500" href={sourceHref}>
         {source || sourceHref}
       </a>
