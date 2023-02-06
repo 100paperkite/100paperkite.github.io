3e294479ee6f054367c557229b8c8a9989b5a344
Baek Jiyeon <100.paperkite@gmail.com>
2023-02-06 15:33:24 +0900

    refactor: 포스팅 데이터 가져오는 중복 코드 분리 및 이에 따른 수정.

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,21 +1,15 @@
 import Layout from './Layout';
-import siteMetadata from '../siteMetadata';
 
-import { Card as PostCard } from './Post';
+import { Card } from './Post';
 
-const HomePage = ({ posts }) => {
+const HomePage = ({ postInfos }) => {
   return (
     <Layout>
       <div className="mt-2">
-        <h3 className="font-bold text-xl md:text-2xl border-neutral-200 border-b pb-2">
-          All Posts
-        </h3>
-        {posts
-          .sort((a, b) => {
-            return a.frontMatter.uploaded < b.frontMatter.uploaded ? 1 : -1;
-          })
-          .map((post, index) => (
-            <PostCard post={post} key={index} />
+        {postInfos
+          .sort((a, b) => (a.uploaded < b.uploaded ? 1 : -1))
+          .map((info, index) => (
+            <Card {...info} key={index} />
           ))}
       </div>
     </Layout>
